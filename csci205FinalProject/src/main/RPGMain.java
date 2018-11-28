168db14907eb25bd6ee9965451a18fa0aeb5d095
Claudia <Claudia@134.82.188.184>
2018-11-28 16:11:27 -0500

    Initial code for JOPtionPane intro screen

--- csci205FinalProject/src/main/RPGMain.java
+++ csci205FinalProject/src/main/RPGMain.java
@@ -19,6 +19,7 @@ import controller.RPGController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 import model.RPGModel;
 import view.RPGView;
 
@@ -41,6 +42,18 @@ public class RPGMain extends Application {
      * Control of the application
      */
     private RPGController theCtrl;
+    /**
+     * Frame for the JOPtionPane intro screen
+     */
+    private JOptionPane frame;
+    /**
+     * Intro message
+     */
+    private String introMessage;
+    /**
+     * Player the user chooses
+     */
+    private String chosenPlayer;
 
     /**
      * Initializes the application
@@ -52,6 +65,16 @@ public class RPGMain extends Application {
     @Override
     public void init() throws Exception {
         super.init();
+
+        frame = new JOptionPane();
+        introMessage = "Welcome to __! Prepare yourself for the most challenging quest of all time : surviving the"
+                       + " class of Computer Science 205 : Software Engineering and Design at Bucknell University. As a student, you"
+                       + " will navigate Bucknell's campus collecting items and talking with people to survive the most"
+                       + " difficult course in Bucknell history. But beware, not all people you encounter will be friendly or willing"
+                       + " to assist you. You must decide for yourself who you should listen to and who you must fight. You will be able"
+                       + " to view your inventory containing your collected items and weapons at all times. Pay close attentiong to"
+                       + " your mental health; failing to do so will result in a failing grade. You pass the class when you defeat "
+                       + "the most dangerous character: your angry professor. Now it's time to choose your player. Good luck!";
 
         theModel = new RPGModel();
         theView = new RPGView(theModel);
@@ -75,6 +98,24 @@ public class RPGMain extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         theCtrl.refresh();
+    }
+
+    /**
+     * Displays the intro screen of the game
+     *
+     * @return String representing name of chosen player
+     */
+    public String introScreen() {
+        JOptionPane.showMessageDialog(frame, introMessage);
+        Object[] characterChoices = {"Kartikeya", "Logan", "Claudia", "Jason"};
+        chosenPlayer = (String) JOptionPane.showInputDialog(frame,
+                                                            "Choose your player: ",
+                                                            "Player Options",
+                                                            JOptionPane.PLAIN_MESSAGE,
+                                                            null,
+                                                            characterChoices,
+                                                            "Kartikeya");
+        return chosenPlayer;
     }
 
     /**
